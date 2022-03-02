2221e560a338be70d98461881aa0976cbcbf7912
1-Accelerator-1 <max.pashuta@mail.ru>
2022-03-02 14:39:11 +0300

    Add project files.

---
+++ Task1/Product.cs
@@ -0,0 +1,31 @@
+﻿namespace Task1
+{
+    public class Product
+    {
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is null)
+            {
+                return false;
+            }
+
+            if (obj.GetType() != typeof(Product))
+            {
+                return false;
+            }
+
+            var product = (Product)obj;
+            return (Name == product.Name) && (Price == product.Price);
+        }
+    }
+}
